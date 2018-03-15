7742916b47f94bd0e9d79b88f426e0d5b024852c
Haris Spahic <spahicharis@users.noreply.github.com>
2018-03-15 23:01:21 +0100

    New route - Statitstika

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,7 @@ const reklame = require('./reklame.route');
 const osobe = require('./osobe.route');
 const auth = require('./auth.route');
 const push = require('./push.route');
+const statistika = require('./statistika.route');
 
 module.exports = function(router, connection, mysql, logger) {
     auth(router, connection, mysql, logger);
@@ -14,4 +15,5 @@ module.exports = function(router, connection, mysql, logger) {
     paketi(router, connection, mysql, logger);
     reklame(router, connection, mysql, logger);
     push(router, connection, mysql, logger);
+    statistika(router, connection, mysql, logger);
 };
---
+++ app/routes/statistika.route.js
@@ -0,0 +1,172 @@
+const models = require('../models');
+const moment = require("moment");
+const _ = require('lodash');
+const helperFunctions = require('../utils/helper-functions');
+const sequelize = require('sequelize');
+module.exports = function (router, connection, mysql, logger) {
+    router.get("/statistika/kupovinaPoGodinama", function (req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        logger.info('GET /statistika/kupovinaPoGodinama');
+
+        const response = {
+            Error: false,
+            Message: 'Success',
+            Desc: 'Kupovina po godinama',
+            Result: []
+        };
+
+        const table = ["kupovina"]
+        let query = 'SELECT YEAR(datumkupovine) as godina, sum(cijena) as cijena, sum(kolicina) as kolicina, count(status) as brojKupovina FROM kupovina WHERE status = '+"'stigao'"+'  group by year(datumkupovine);'
+        query = mysql.format(query, table);
+        connection.getConnection(function (err, conn) {
+            conn.query(query, function (err, results) {
+                if (err) {
+                    logger.error(err);
+                    res.json({ "Error": true, "Message": err });
+                }
+                else {
+                    response.Result = results;
+                    res.json(response);
+                }
+                conn.release();
+            });
+        });
+    });
+
+    router.get("/statistika/kupovinaPoMjesecima", function (req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        logger.info('GET /statistika/kupovinaPoMjesecima');
+
+        const response = {
+            Error: false,
+            Message: 'Success',
+            Desc: 'Kupovina po mjesecima za sve godine',
+            Result: []
+        };
+
+        const table = ["kupovina"]
+        let query = 'SELECT year(datumkupovine) as godina, month(datumkupovine) as mjesec, sum(cijena) as cijena, sum(kolicina) as kolicina, count(status) as brojKupovina FROM kupovina WHERE status = '+"'stigao'"+'  group by year(datumkupovine),month(datumkupovine);'
+        query = mysql.format(query, table);
+        connection.getConnection(function (err, conn) {
+            conn.query(query, function (err, results) {
+                if (err) {
+                    logger.error(err);
+                    res.json({ "Error": true, "Message": err });
+                }
+                else {
+                    response.Result = results;
+                    res.json(response);
+                }
+                conn.release();
+            });
+        });
+    });
+
+
+    router.get("/statistika/prodajaPoGodinama", function (req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        logger.info('GET /statistika/prodajaPoGodinama');
+
+        const response = {
+            Error: false,
+            Message: 'Success',
+            Desc: 'Prodaja po godinama',
+            Result: []
+        };
+
+        const table = ["prodaja"]
+        let query = 'SELECT YEAR(datum) as godina, sum(cijena) as cijena, count(status) as brojProdaja FROM prodaja WHERE status = '+"'Prodano'"+'  group by year(datum);'
+        query = mysql.format(query, table);
+        connection.getConnection(function (err, conn) {
+            conn.query(query, function (err, results) {
+                if (err) {
+                    logger.error(err);
+                    res.json({ "Error": true, "Message": err });
+                }
+                else {
+                    response.Result = results;
+                    res.json(response);
+                }
+                conn.release();
+            });
+        });
+    });
+
+
+    router.get("/statistika/prodajaPoMjesecima", function (req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        logger.info('GET /statistika/prodajaPoGodinama');
+
+        const response = {
+            Error: false,
+            Message: 'Success',
+            Desc: 'Prodaja po mjesecima za sve godine',
+            Result: []
+        };
+
+        const table = ["prodaja"]
+        let query = 'SELECT YEAR(datum) as godina, month(datum) as mjesec, sum(cijena) as cijena, count(status) as brojProdaja FROM prodaja WHERE status = '+"'Prodano'"+'  group by year(datum), month(datum);'
+        query = mysql.format(query, table);
+        connection.getConnection(function (err, conn) {
+            conn.query(query, function (err, results) {
+                if (err) {
+                    logger.error(err);
+                    res.json({ "Error": true, "Message": err });
+                }
+                else {
+                    response.Result = results;
+                    res.json(response);
+                }
+                conn.release();
+            });
+        });
+    });
+
+    router.get("/statistika/finansije", function (req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        logger.info('GET /statistika/finansije');
+
+        const response = {
+            Error: false,
+            Message: 'Success',
+            Desc: 'Finasije (kupljeno/prodano)',
+            Result: []
+        };
+
+        const table = ["kupovina", "prodaja"];
+        let query = 'SELECT sum(cijena) as kupljeno FROM kupovina;'
+        let query2 = 'SELECT sum(cijena) as prodano FROM prodaja;'
+        let query3 = 'SELECT sum(cijenaReklame) as cijenaReklama FROM reklame;'
+        query = mysql.format(query, table);
+        query2 = mysql.format(query2, table);
+        query3 = mysql.format(query3, table);
+        connection.getConnection(function (err, conn) {
+            conn.query(query + query2 + query3, function (err, results) {
+                if (err) {
+                    logger.error(err);
+                    res.json({ "Error": true, "Message": err });
+                }
+                else {
+                    response.Result = [Object.assign({}, results[0][0], results[1][0], results[2][0],{zarada: results[1][0].prodano - (results[0][0].kupljeno + results[2][0].cijenaReklama)})];
+                    res.json(response);
+                }
+                conn.release();
+            });
+        });
+    });
+};
